40184b9531568fecd48c3a0e36767dc7759eb738
Toshiki Iga <tosiki.iga@nifty.ne.jp>
2015-10-12 06:58:48 +0900

    set debug as true.

--- blanco/main/blanco/batchprocess/BlancoBatchProcessConstants.java
+++ blanco/main/blanco/batchprocess/BlancoBatchProcessConstants.java
@@ -14,7 +14,7 @@ public class BlancoBatchProcessConstants extends AbstractBlancoBatchProcessConst
      *
      * [@BlancoConstantsVersion]
      */
-    public static final String VERSION = "0.6.2-I201510120642";
+    public static final String VERSION = "0.6.2-I201510120657";
 
     /**
      * Getter for version constants.
