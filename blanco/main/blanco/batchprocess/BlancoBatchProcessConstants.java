bf7a6b836b6c17b4a3e74c8300059a849767a1ac
Toshiki Iga <tosiki.iga@nifty.ne.jp>
2015-10-12 21:57:17 +0900

    Change version number.

--- blanco/main/blanco/batchprocess/BlancoBatchProcessConstants.java
+++ blanco/main/blanco/batchprocess/BlancoBatchProcessConstants.java
@@ -14,7 +14,7 @@ public class BlancoBatchProcessConstants extends AbstractBlancoBatchProcessConst
      *
      * [@BlancoConstantsVersion]
      */
-    public static final String VERSION = "0.6.2-I201510120657";
+    public static final String VERSION = "0.6.2-I201510122155";
 
     /**
      * Getter for version constants.
