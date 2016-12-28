ca2b5eea91b842e364f33e4542545731dc66bcce
Toshiki Iga <igapyon@gmail.com>
2016-12-28 18:13:49 +0900

    save

--- src/test/java/blanco/sample/batchprocess/Sample2BatchProcessTest.java
+++ src/test/java/blanco/sample/batchprocess/Sample2BatchProcessTest.java
@@ -9,7 +9,7 @@ public class Sample2BatchProcessTest extends TestCase {
             // このJUnitを実行すると、内部的に System.exitが実行されます。
             final String[] args = { "-require=必須項目", "-field_int_req=123",
                     "-field_long_req=456", "-field_decimal_req=123.456" };
-            Sample2BatchProcess.main(args);
+//            Sample2BatchProcess.main(args);
         }
 
         if (false) {
@@ -17,7 +17,7 @@ public class Sample2BatchProcessTest extends TestCase {
             final String[] args = { "-require=必須項目", "-field_int_req=123",
                     "-field_long_req=456", "-field_decimal_req=パース不能",
                     "-field_boolean_req=false" };
-            Sample2BatchProcess.main(args);
+  //          Sample2BatchProcess.main(args);
         }
     }
 
