872662080d16180ac7e5813a55b0f71844b809b2
tueda <ueo@ueo.co.jp>
2020-10-06 19:39:25 +0900

    preparing to enable compile without blanco2g runtimes.

--- blanco/main/blanco/batchprocess/BlancoBatchProcessConstants.java
+++ blanco/main/blanco/batchprocess/BlancoBatchProcessConstants.java
@@ -11,7 +11,7 @@ public class BlancoBatchProcessConstants extends AbstractBlancoBatchProcessConst
      *
      * [@BlancoConstantsVersion]
      */
-    public static final String VERSION = "0.6.2-I201510122155";
+    public static final String VERSION = "2.1.0-beta";
 
     /**
      * Getter for version constants.
